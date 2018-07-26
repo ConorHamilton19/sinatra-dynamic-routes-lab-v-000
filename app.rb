bf6956f1efa3f3ec815ed2e0365ad0f5800fd029
Conor Hamilton <conhamil19@gmail.com>
2018-07-26 00:34:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -33,6 +33,13 @@ class App < Sinatra::Base
    
    case params[:operation]
    
-   when add 
+   when 'add'
+     "#{@number1 + @number2}"
+   when 'subtract'
+     "#{@number1 - @number2}"
+   when 'multiply'
+     "#{@number1 * @number2}"
+   when 'divide'
+     "#{@number1@number2}"
  end 
 end
